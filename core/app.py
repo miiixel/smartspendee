823b79dee5f449e0f0b491ba108049783f721a42
miiixel <kurtsabater@gmail.com>
2025-07-16 21:36:45 +0800

    Updated Flask Codebase

---
+++ core/app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "SmartSpendee Home Page"
+
+@app.route("/add-income")
+def add_income():
+    return "Add monthly income"
+
+@app.route("/add-expense")
+def add_expense():
+    return "Add or edit expense"
+
+@app.route("/view-expense")
+def view_expenses():
+    return "List of expenses"
+
+@app.route("/stats")
+def view_stats():
+    return "Budget statistics"
