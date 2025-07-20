b50195bfabd116f231db8ed3c2e3908cc2c9be17
kurt andrew <kurtsabater@gmail.com>
2025-07-20 18:07:55 +0800

    Rewrite Codebase into Flask program

--- smartspendee/app.py
+++ smartspendee/app.py
@@ -1,26 +1,38 @@
 from flask import Flask, render_template, request, redirect, url_for
-
+import features as features
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/add-income", methods=['GET', 'POST'])
-def add_income():
+@app.route("/add-income", methods=["GET", "POST"])
+def addIncome():
     if request.method == "POST":
         income = request.form["income"]
-        return f"Income of {income} saved!"
+        features.saveIncome(income)
+        return '''
+        <h3>Income saved!</h3>
+        <a href="/">Back to home page</a>
+        '''
     return render_template("add-income.html")
 
-@app.route("/add-expense")
+@app.route("/add-expense", methods=["GET", "POST"])
 def add_expense():
-    return "Add or edit expense"
+    if request.method == "POST":
+        item = request.form["item"]
+        amount = request.form["amount"]
+        category = request.form["category"]
+        features.addExpense(item, amount, category)
+        return redirect(url_for("add_expense_confirm"))
 
-@app.route("/view-expense")
-def view_expenses():
-    return "List of expenses"
+    return render_template("add-expense.html")
 
-@app.route("/stats")
-def view_stats():
-    return "Budget statistics"
+@app.route("/add-expense/confirm")
+def add_expense_confirm():
+    return '''
+        <h3>Expense saved!</h3>
+        <p>Would you like to add another expense?</p>
+        <a href="/add-expense">Add another expense</a><br>
+        <a href="/">Back to home page</a>
+    '''
